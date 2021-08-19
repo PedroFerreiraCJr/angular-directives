1afc4b7c4bb3713fb982578369e1424e2763f44c
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-19 17:32:58 -0300

    aula sobre a diretiva ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.componen
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
     DiretivaNgforComponent,
+    DiretivaNgclassComponent,
   ],
   imports: [
     BrowserModule,
